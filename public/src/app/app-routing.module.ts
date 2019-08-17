324126caf2964ed80a3159a08930dac390a296f1
Carlos Crispin <carloscrispin@MacBook-Air-de-Carlos.local>
2019-08-16 20:59:01 -0500

    c1

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './default/header/header.component';
 import { UserComponent } from './user/user.component';
-import { UserListComponent } from "./user/user-list/user-list.component";
 
 
 
 const routes: Routes = [ 
-  {path : '', component : HeaderComponent,},
-  {path : 'productos', component : UserComponent},
+  {path : '', component: HeaderComponent, pathMatch:"full"},
+  {path : 'productos', component: UserComponent},
   // {path : 'nuevo', component : UserComponent},
-  {path : '**', component : HeaderComponent,},
+  // {path : '**', component : HeaderComponent}
 ];
 // const routes: Route[] =[
 //   {path :'',component:HolaComponent},
